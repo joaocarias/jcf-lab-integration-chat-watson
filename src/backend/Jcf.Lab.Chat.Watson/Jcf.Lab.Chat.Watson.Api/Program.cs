5057110b9778e17ddc362aaf7ac26f66ec15238d
Joao Franca <jfranca@omnize.com>
2026-06-26 16:15:21 -0300

    Resolvendo o problema de visibilidade do controller no backend

--- src/backend/Jcf.Lab.Chat.Watson/Jcf.Lab.Chat.Watson.Api/Program.cs
+++ src/backend/Jcf.Lab.Chat.Watson/Jcf.Lab.Chat.Watson.Api/Program.cs
@@ -7,7 +7,11 @@ builder.Services.AddOpenApi();
 builder.Services.Configure<WatsonEmbedOptions>(
     builder.Configuration.GetSection(WatsonEmbedOptions.SectionName));
 
+builder.Services.AddControllers();
+
 var app = builder.Build();
+
+app.MapControllers();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -15,30 +19,9 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
+if (!app.Environment.IsProduction())
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    app.UseHttpsRedirection();
+}
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
